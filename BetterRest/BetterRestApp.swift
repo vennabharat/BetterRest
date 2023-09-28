c04f7544aa854fd7ec8079cb64b24df7dab3e114
Venna Bharat <v.bharath39@gmail.com>
2023-09-28 14:52:42 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by bharat venna on 28/09/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
